206060834b9d36698ed9cc48d35517b4ed391a67
why <why3046380@163.com>
2015-09-14 18:54:30 +0800

    000

--- src/main/java/com/iezu/controller/UserController.java
+++ src/main/java/com/iezu/controller/UserController.java
@@ -3,6 +3,7 @@ package com.iezu.controller;
 import com.iezu.common.ResultModel;
 import com.iezu.domain.UserInfo;
 import com.sun.deploy.net.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,18 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by 20150831 on 2015/9/13.
  */
 @Controller
 public class UserController {
+    @Autowired
+    private DataSource dataSource;
     @RequestMapping("/user/index")
     public ModelAndView index(){
         ModelAndView view=new ModelAndView("user/index");
@@ -27,7 +34,10 @@ public class UserController {
     }
     @RequestMapping(value = "/user/adduser")
     @ResponseBody
-    public ResultModel UserAdd(@RequestParam String username,@RequestParam String password,HttpSession session){
+    public ResultModel UserAdd(@RequestParam String username,@RequestParam String password,HttpSession session) throws SQLException {
+        Connection connection=dataSource.getConnection();
+        PreparedStatement statement= connection.prepareStatement("");
+        statement.execute();
         UserInfo demo=new UserInfo();
         demo.setUsername(username);
         demo.setPassword(password);
